b0f9c00f581144f6fa0dd310ab87a7f9c901e606
云墨款哥 <15239111596@163.com>
2025-06-18 20:49:38 +0800

    初始化demo，准备开始组件化demo

--- src/components/List.vue
+++ src/components/List.vue
@@ -18,34 +18,6 @@ export default {
             default: () => []
         }
     },
-    beforeCreate() {
-        console.log('子组件-生命周期函数：beforeCreate', this.$el, this.$data);
-        // 生命周期函数：beforeCreate undefined undefined
-    },
-    created() {
-        console.log('子组件-生命周期函数：created');
-        // 生命周期函数：created undefined {__ob__: Observer}
-    },
-    beforeMount() {
-        console.log('子组件-生命周期函数：beforeMount');
-        // 生命周期函数：beforeMount undefined {__ob__: Observer}
-    },
-    mounted() {
-        console.log('子组件-生命周期函数：mounted');
-        // 生命周期函数：mounted <div id=​"app">​…​</div>​ {__ob__: Observer}
-    },
-    beforeUpdate() {
-        console.log('子组件-生命周期函数：beforeUpdate');
-    },
-    updated() {
-        console.log('子组件-生命周期函数：updated');
-    },
-    beforeDestroy() {
-        console.log('子组件-生命周期函数：beforeDestroy');
-    },
-    destroyed() {
-        console.log('子组件-生命周期函数：destroyed');
-    },
 }
 </script>
 
